12ffb0948af49e116da04530ac073f75772d87fc
Itamar Fisch <72417568+ItamarFi@users.noreply.github.com>
2022-04-25 10:42:47 +0300

    Update Command.cpp
    Display on main window

--- Command.cpp
+++ Command.cpp
@@ -22,7 +22,6 @@ const gchar* ICommand::getDescription() {
     return this->description;
 };
 
-
 CommandStart::CommandStart (VideoStream *stream) : ICommand(stream) {
     this->mainstream = stream;
     this->description = "Start playing";
@@ -32,118 +31,174 @@ void CommandStart::execute() {
     this->mainstream->startPlaying();
 }
 
-
-class CommandStop : public ICommand
-{
-public:
-    CommandStop(VideoStream *stream) : ICommand(stream) {
-        this->mainstream = stream;
-        this->description = "Stop playing";
+CommandStop::CommandStop (VideoStream *stream) : ICommand(stream) {
+    this->mainstream = stream;
+    this->description = "Stop playing";
     }
-    void execute() {
-        mainstream->stopPlaying();
+
+void CommandStop::execute() {
+      mainstream->stopPlaying();
+}
+
+CommandPause::CommandPause (VideoStream *stream) : ICommand(stream) {
+    this->mainstream = stream;
+    this->description = "Pause playing";
     }
-};
 
-class CommandPause : public ICommand
-{
-public:
-    CommandPause(VideoStream *stream) : ICommand(stream) {
-        this->mainstream = stream;
-        this->description = "Pause playing";
-    }
-    void execute() {
-        mainstream->pausePlaying();
-    }
-};
+void CommandPause::execute() {
+      mainstream->pausePlaying();
+}
 
-
-class CommandConnectCam : public ICommand
-{
-private:
-  SubVideoStream* webcam;  
-
-public:
-    CommandConnectCam(VideoStream *stream) : ICommand(stream) {
-        this->mainstream = stream;
-        this->description = "Connect to Webcam";
+CommandConnectCam::CommandConnectCam (VideoStream *stream) : ICommand(stream) {
+      this->mainstream = stream;
+      this->description = "Connect to Webcam";
         
-        std::vector<GstElement*> webcam_elements;
-        webcam_elements.push_back(gst_element_factory_make ("v4l2src", "webcam_source"));
-        
-        const gchar *webcam_name = "webcam";
-        this->webcam = new SubVideoStream(webcam_elements, webcam_name);
+      std::vector<GstElement*> webcam_elements;
+      webcam_elements.push_back(gst_element_factory_make ("v4l2src", "webcam_source"));
+      
+      const gchar *webcam_name = "webcam";
+      this->webcam = new SubVideoStream(webcam_elements, webcam_name);
     }
-    void execute() {
-        this->mainstream->addSubStream(this->webcam, GST_PAD_SRC);
-    }
-};
 
+void CommandConnectCam::execute() {
+    this->mainstream->addSubStream(this->webcam, GST_PAD_SRC);
+}
 
-class CommandRecord : public ICommand
-{
-private:
-  SubVideoStream* record;  
+CommandRecord::CommandRecord  (VideoStream *stream) : ICommand(stream) {
+      this->mainstream = stream;
+      this->description = "Start record";
 
-public:
-    CommandRecord(VideoStream *stream) : ICommand(stream) {
-        this->mainstream = stream;
-        this->description = "Start record";
-
-        std::vector<GstElement*> record_elements;
-        record_elements.push_back(gst_element_factory_make("queue", "queue_record"));
-        record_elements.push_back(gst_element_factory_make("videoconvert", NULL));
-        record_elements.push_back(gst_element_factory_make("videoscale", NULL));
-        record_elements.push_back(gst_element_factory_make("videorate", NULL));
-        record_elements.push_back(gst_element_factory_make("theoraenc", NULL));
-        record_elements.push_back(gst_element_factory_make("oggmux", NULL));
-        record_elements.push_back(gst_element_factory_make ("filesink", "sink_record"));
+      std::vector<GstElement*> record_elements;
+      record_elements.push_back(gst_element_factory_make("queue", "queue_record"));
+      record_elements.push_back(gst_element_factory_make("videoconvert", NULL));
+      record_elements.push_back(gst_element_factory_make("videoscale", NULL));
+      record_elements.push_back(gst_element_factory_make("videorate", NULL));
+      record_elements.push_back(gst_element_factory_make("theoraenc", NULL));
+      record_elements.push_back(gst_element_factory_make("oggmux", NULL));
+      record_elements.push_back(gst_element_factory_make ("filesink", "sink_record"));
 	    g_object_set(record_elements[6], "location", "record.mp4", NULL);
 
-        const gchar *record_name = "record";
-        this->record = new SubVideoStream(record_elements, record_name);
-    }
-    void execute() override {
-        this->mainstream->addSubStream(this->record, GST_PAD_SINK);
-    }
-};
+      const gchar *record_name = "record";
+      this->record = new SubVideoStream(record_elements, record_name);
+  }
 
-class CommandDisplay : public ICommand
-{
-private:
-  SubVideoStream* display;  
+void CommandRecord::execute() {
+    this->mainstream->addSubStream(this->record, GST_PAD_SINK);
+}
 
-public:
-    CommandDisplay(VideoStream *stream) : ICommand(stream) {
+CommandDisplay::CommandDisplay(VideoStream *stream) : ICommand(stream) {
         this->mainstream = stream;
         this->description = "Start display";
 
         std::vector<GstElement*> display_elements;
         display_elements.push_back(gst_element_factory_make("queue", "queue_display"));
         display_elements.push_back(gst_element_factory_make("xvimagesink", "sink_display"));
+        // display_elements.push_back(gst_element_factory_make("glimagesink", "sink_display"));
 
         const gchar *display_name = "display";
         this->display = new SubVideoStream(display_elements, display_name);
     }
-    void execute() {
+
+void CommandDisplay::execute() {
         this->mainstream->addSubStream(this->display, GST_PAD_SINK);
-    }
-};
+}
 
-class CommandStatus : public ICommand
-{
-private:
-  SubVideoStream* display;  
-
-public:
-    CommandStatus(VideoStream *stream) : ICommand(stream) {
-        this->mainstream = stream;
-        this->description = "Print status of VideoStream";
-    }
-    void execute() {
-        g_print("\n===========================\n");
-        g_print("Status:\n");
+CommandStatus::CommandStatus(VideoStream *stream) : ICommand(stream) {
+    this->mainstream = stream;
+    this->description = "Print status of VideoStream";
+}
+void CommandStatus::execute() {
+        g_print("%s", "\n===========================\n");
+        g_print("%s", "Status:\n");
         this->mainstream->printStatus();
-        g_print("===========================\n");
+        g_print("%s", "===========================\n");
+}
+
+/*
+print list of devices 
+*/
+std::string printDevicesNames () {
+  GstDeviceMonitor* monitor = gst_device_monitor_new();
+  if (!gst_device_monitor_start(monitor)){
+    printf("WARNING: Monitor couldn't started.\n");
+  }
+
+  // get list of GstDevice*
+  GList *devices = gst_device_monitor_get_devices(monitor);
+  gchar *device_name;
+  
+  g_printerr("%s", "\nList of available devices:\n\n");
+
+  // iterate over the list and print the names of the devices
+  while (devices != NULL)
+  {
+    device_name = gst_device_get_display_name((GstDevice *)devices->data);
+    g_print("%s", device_name);
+    g_print("%s", "\n");
+
+    devices = devices->next;
+  }
+  g_print("%s", "\n");
+
+  // free resources
+  devices = g_list_first(devices);
+  g_list_free_full(devices, NULL);
+  gst_device_monitor_stop(monitor);
+}
+
+/*
+get name (full or particial) of device and return its port
+*/
+void findSourcePortByName (std::string name) {
+
+  bool found = FALSE;
+
+  GstDeviceMonitor* monitor = gst_device_monitor_new();
+  if (!gst_device_monitor_start(monitor)){
+    printf("%s", "WARNING: Monitor couldn't started.\n");
+  }
+
+  GList *devices = gst_device_monitor_get_devices(monitor);
+  gchar *device_name;
+  gchar *device_port;
+  std::string d_str;
+
+// print title
+  g_print("%s", "Devices found with substring \"");
+  g_print("%s", name.c_str());
+  g_print("%s", "\":\n\n");
+
+// iterate over the list and look for compatible devices
+  while (devices != NULL)
+  {
+    // compare names
+    device_name = gst_device_get_display_name((GstDevice *)devices->data);
+    if (d_str.assign(device_name).find(name) != std::string::npos) {
+      // success. print details 
+      found = TRUE;
+      GstStructure *gstruct = gst_device_get_properties((GstDevice *)devices->data);
+      const gchar *field_name = "device.path";
+
+      g_print("%s", "Device name: ");
+      g_print("%s", device_name);
+      g_print("%s", "\n");
+
+      g_print("%s", "Device port: ");
+      g_print("%s", gst_structure_get_string(gstruct, field_name));
+      g_print("%s", "\n\n");
+
+      gst_structure_free(gstruct);
     }
-};
+    devices = devices->next;
+  }
+
+  // free resources
+  devices = g_list_first(devices);
+  g_list_free_full(devices, NULL);
+  gst_device_monitor_stop(monitor);
+
+  // device not found notice
+  if (!found) {
+    g_print("%s", "Sorry. Device not found.\n");
+  }
+}
